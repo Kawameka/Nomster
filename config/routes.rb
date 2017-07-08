24d8ff592b00e55a29ce85fb296b3fa94464ba35
Tanner <tanner.tannerlevine@gmail.com>
2017-07-08 23:08:40 +0000

    created controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #includes the index action when using rake routes in command line
+  root 'places#index'
 end
