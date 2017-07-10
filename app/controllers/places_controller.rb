d1415f5ca4dd1ead612741a8c48a70b0161ffbfe
Tanner <tanner.tannerlevine@gmail.com>
2017-07-10 22:20:43 +0000

    connected new place page to db

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,6 +8,19 @@ class PlacesController < ApplicationController
     def new 
         @place = Place.new
     end
+
+    # creates a db entry for user entered place
+    def create
+        Place.create(place_params)
+        redirect_to root_path
+    end
     
+    private
+
+    # method that allows user submitted name, description, and address
+    # to be added to new place db entry
+    def place_params
+        params.require(:place).permit(:name, :description, :address)
+    end
     
 end
