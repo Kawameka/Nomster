7ae9a3495e16d717e5e4690befbef2e0ff4b27fb
Tanner <tanner.tannerlevine@gmail.com>
2017-07-13 21:28:30 +0000

    secured edit page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
     # requires that the user is signed up before they can create a new place entry
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
     def index
         @places = Place.paginate(:page => params[:page], :per_page => 5)
@@ -24,10 +24,18 @@ class PlacesController < ApplicationController
 
     def edit
         @place = Place.find(params[:id])
+
+        if @place.user != current_user
+            return render text: 'Not Allowed', status: :forbidden
+        end
+        
     end
 
     def update
         @place = Place.find(params[:id])
+        if @place.user != current_user
+            return render text: 'Not Allowed', status: :forbidden
+        end
         @place.update_attributes(place_params)
         redirect_to root_path
     end
