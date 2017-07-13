5daf6e50ea7f9c13b2cc4845804672b53f2f1318
Tanner <tanner.tannerlevine@gmail.com>
2017-07-13 21:30:44 +0000

    secured place deletion

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
     # requires that the user is signed up before they can create a new place entry
-    before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
     def index
         @places = Place.paginate(:page => params[:page], :per_page => 5)
@@ -42,6 +42,9 @@ class PlacesController < ApplicationController
 
     def destroy
         @place = Place.find(params[:id])
+        if @place.user != current_user
+            return render text: 'Not Allowed', status: :forbidden
+        end
         @place.destroy
         redirect_to root_path
     end
