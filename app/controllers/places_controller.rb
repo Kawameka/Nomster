6c8d6b56da93a874f0dd1a11eea9e9284a8f1179
Tanner <tanner.tannerlevine@gmail.com>
2017-07-13 04:39:16 +0000

    set up individual place page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,6 +16,11 @@ class PlacesController < ApplicationController
         current_user.places.create(place_params)
         redirect_to root_path
     end
+
+    def show
+        @place = Place.find(params[:id])
+    end
+    
     
     private
 
